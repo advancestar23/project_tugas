aca052d5aad834c9bc833992fc78bb74b5960a8f
advancestar23 <advancestar18@gmail.com>
2025-03-03 08:11:54 +0700

    tambahan file anggota3.js

---
+++ fileanggota3.js
@@ -0,0 +1,11 @@
+function cekGenapGanjil(angka) {
+    if (angka % 2 === 0) {
+        console.log(angka + " adalah angka genap.");
+    } else {
+        console.log(angka + " adalah angka ganjil.");
+    }
+}
+
+// Contoh penggunaan
+cekGenapGanjil(10); 
+cekGenapGanjil(7);  
